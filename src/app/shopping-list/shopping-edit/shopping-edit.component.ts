eb8016a4b08264cf332d57dfce8245941615af17
Nazri Ghazi <nazrighazi02@gmail.com>
2022-04-11 08:37:04 +0800

    shopping-recipe init

---
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -0,0 +1,34 @@
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
+
+@Component({
+  selector: 'app-shopping-edit',
+  templateUrl: './shopping-edit.component.html',
+  styleUrls: ['./shopping-edit.component.css'],
+})
+export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput', { static: false }) nameInput: ElementRef;
+  @ViewChild('amountInput', { static: false }) amountInput: ElementRef;
+
+  @Output() addedIngredient = new EventEmitter<Ingredient>();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onAddIngredient() {
+    this.addedIngredient.emit(
+      new Ingredient(
+        this.nameInput.nativeElement.value,
+        this.amountInput.nativeElement.value
+      )
+    );
+  }
+}
